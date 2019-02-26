259bae5fca997299c7c5f8b1b442f293f5ff333b
Vladimir Kovalchuk <Vladimir6332@gmail.com>
2019-02-27 01:36:50 +0300

    Added feauture for counting lover triangles

--- src/index.js
+++ src/index.js
@@ -3,5 +3,22 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+    let counter1 = 0;
+    let counter2 = 0;
+    for (let i = 1; i <= preferences.length; i++) {
+
+        let pichonees1Lover = i;
+        let pichonees2Lover = preferences[pichonees1Lover - 1];
+        let pichonees3Lover = preferences[pichonees2Lover - 1];
+        if (preferences[pichonees3Lover - 1] == pichonees1Lover)
+            counter1++;
+        if (counter1 == 3) {
+            counter2++;
+            counter1 = 0;
+        }
+
+
+
+    }
+    return counter2
 };
